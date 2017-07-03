efe846b9a103fc92b7aea6f76b3272c613238dc6
Daniel Hansson <daniel@techandme.se>
2017-07-03 19:23:13 +0200

    refactor

--- wordpress_install.sh
+++ wordpress_install.sh
@@ -31,158 +31,203 @@ STATIC="https://raw.githubusercontent.com/techandme/wordpress-vm/master/static"
 UNIXUSER=wordpress
 UNIXPASS=wordpress
 
-# Check if root
-        if [ "$(whoami)" != "root" ]; then
-        echo
-        echo -e "\e[31mSorry, you are not root.\n\e[0mYou must type: \e[36msudo \e[0mbash $SCRIPTS/wordpress_install.sh"
-        echo
-        exit 1
-fi
+#!/bin/bash
+
+# Tech and Me © - 2017, https://www.techandme.se/
 
 # Prefer IPv4
 sed -i "s|#precedence ::ffff:0:0/96  100|precedence ::ffff:0:0/96  100|g" /etc/gai.conf
 
-# Check Ubuntu version
-echo "Checking server OS and version..."
-if [ $OS -eq 1 ]
+# shellcheck disable=2034,2059
+true
+# shellcheck source=lib.sh
+FIRST_IFACE=1 && CHECK_CURRENT_REPO=1 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+unset FIRST_IFACE
+unset CHECK_CURRENT_REPO
+
+# Check for errors + debug code and abort if something isn't right
+# 1 = ON
+# 0 = OFF
+DEBUG=0
+debug_mode
+
+# Check if root
+if ! is_root
 then
-        sleep 1
-else
-        echo "Ubuntu Server is required to run this script."
-        echo "Please install that distro and try again."
-        exit 1
+    printf "\n${Red}Sorry, you are not root.\n${Color_Off}You must type: ${Cyan}sudo ${Color_Off}bash %s/nextcloud_install_production.sh\n" "$SCRIPTS"
+    exit 1
 fi
 
-DISTRO=$(lsb_release -sd | cut -d ' ' -f 2)
-version(){
-    local h t v
+# Test RAM size (2GB min) + CPUs (min 1)
+ram_check 2 Nextcloud
+cpu_check 1 Nextcloud
 
-    [[ $2 = "$1" || $2 = "$3" ]] && return 0
+# Show current user
+echo
+echo "Current user with sudo permissions is: $UNIXUSER".
+echo "This script will set up everything with that user."
+echo "If the field after ':' is blank you are probably running as a pure root user."
+echo "It's possible to install with root, but there will be minor errors."
+echo
+echo "Please create a user with sudo permissions if you want an optimal installation."
+run_static_script adduser
 
-    v=$(printf '%s\n' "$@" | sort -V)
-    h=$(head -n1 <<<"$v")
-    t=$(tail -n1 <<<"$v")
+# Check Ubuntu version
+echo "Checking server OS and version..."
+if [ "$OS" != 1 ]
+then
+    echo "Ubuntu Server is required to run this script."
+    echo "Please install that distro and try again."
+    exit 1
+fi
 
-    [[ $2 != "$h" && $2 != "$t" ]]
-}
 
 if ! version 16.04 "$DISTRO" 16.04.4; then
     echo "Ubuntu version $DISTRO must be between 16.04 - 16.04.4"
     exit
 fi
 
+# Check if key is available
+if ! wget -q -T 10 -t 2 "$NCREPO" > /dev/null
+then
+    echo "Nextcloud repo is not available, exiting..."
+    exit 1
+fi
+
 # Check if it's a clean server
-echo "Checking if it's a clean server..."
-if [ $(dpkg-query -W -f='${Status}' mysql-common 2>/dev/null | grep -c "ok installed") -eq 1 ];
-then
-        echo "MySQL is installed, it must be a clean server."
-        exit 1
-fi
-
-if [ $(dpkg-query -W -f='${Status}' apache2 2>/dev/null | grep -c "ok installed") -eq 1 ];
-then
-        echo "Apache2 is installed, it must be a clean server."
-        exit 1
-fi
-
-if [ $(dpkg-query -W -f='${Status}' php 2>/dev/null | grep -c "ok installed") -eq 1 ];
-then
-        echo "PHP is installed, it must be a clean server."
-        exit 1
-fi
-
-# Create $UNIXUSER if not existing
-if id "$UNIXUSER" >/dev/null 2>&1
-then
-        echo "$UNIXUSER already exists!"
-else
-        adduser --disabled-password --gecos "" $UNIXUSER
-        echo -e "$UNIXUSER:$UNIXPASS" | chpasswd
-        usermod -aG sudo $UNIXUSER
-fi
-
-if [ -d /home/$UNIXUSER ];
-then
-        echo "$UNIXUSER OK!"
-else
-        echo "Something went wrong when creating the user... Script will exit."
-        exit 1
-fi
+is_this_installed postgresql
+is_this_installed apache2
+is_this_installed php
+is_this_installed mysql-common
+is_this_installed mariadb-server
 
 # Create $SCRIPTS dir
-      	if [ -d $SCRIPTS ]; then
-      		sleep 1
-      		else
-      	mkdir $SCRIPTS
+if [ ! -d "$SCRIPTS" ]
+then
+    mkdir -p "$SCRIPTS"
 fi
 
+# Change DNS
+if ! [ -x "$(command -v resolvconf)" ]
+then
+    apt install resolvconf -y -q
+    dpkg-reconfigure resolvconf
+fi
+echo "nameserver 8.8.8.8" > /etc/resolvconf/resolv.conf.d/base
+echo "nameserver 8.8.4.4" >> /etc/resolvconf/resolv.conf.d/base
+
 # Check network
-sudo ifdown $IFACE && sudo ifup $IFACE
-nslookup google.com
-if [[ $? > 0 ]]
+if ! [ -x "$(command -v nslookup)" ]
+then
+    apt install dnsutils -y -q
+fi
+if ! [ -x "$(command -v ifup)" ]
+then
+    apt install ifupdown -y -q
+fi
+sudo ifdown "$IFACE" && sudo ifup "$IFACE"
+if ! nslookup google.com
 then
     echo "Network NOT OK. You must have a working Network connection to run this script."
-    exit
-else
-    echo "Network OK."
+    exit 1
 fi
 
 # Set locales
 apt install language-pack-en-base -y
 sudo locale-gen "sv_SE.UTF-8" && sudo dpkg-reconfigure --frontend=noninteractive locales
 
-# Change DNS
-echo "nameserver 8.26.56.26" > /etc/resolvconf/resolv.conf.d/base
-echo "nameserver 8.20.247.20" >> /etc/resolvconf/resolv.conf.d/base
-
 # Check where the best mirrors are and update
-echo "Locating the best mirrors..."
-apt update -q2
-apt install python-pip -y
-pip install \
-    --upgrade pip \
-    apt-select
-apt-select
-sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup && \
-sudo mv sources.list /etc/apt/
+echo
+printf "Your current server repository is:  ${Cyan}%s${Color_Off}\n" "$REPO"
+if [[ "no" == $(ask_yes_or_no "Do you want to try to find a better mirror?") ]]
+then
+    echo "Keeping $REPO as mirror..."
+    sleep 1
+else
+   echo "Locating the best mirrors..."
+   apt update -q4 & spinner_loading
+   apt install python-pip -y
+   pip install \
+       --upgrade pip \
+       apt-select
+    apt-select -m up-to-date -t 5 -c
+    sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup && \
+    if [ -f sources.list ]
+    then
+        sudo mv sources.list /etc/apt/
+    fi
+fi
 clear
 
+# Set keyboard layout
+echo "Current keyboard layout is $(localectl status | grep "Layout" | awk '{print $3}')"
+if [[ "no" == $(ask_yes_or_no "Do you want to change keyboard layout?") ]]
+then
+    echo "Not changing keyboard layout..."
+    sleep 1
+    clear
+else
+    dpkg-reconfigure keyboard-configuration
+    clear
+fi
+
 # Update system
-apt update -q2
+apt update -q4 & spinner_loading
 
-# Install packages for Webmin
-apt install -y zip perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
+# Write MARIADB pass to file and keep it safe
+cat << LOGIN > "$MYCNF"
+[client]
+password='$MARIADB_PASS'
+default-character-set = utf8mb4
 
-# Install Webmin
-sed -i '$a deb http://download.webmin.com/download/repository sarge contrib' /etc/apt/sources.list
-wget -q http://www.webmin.com/jcameron-key.asc -O- | sudo apt-key add -
-apt update -q2
-apt install webmin -y
+[mariadb]
+innodb_use_fallocate = 1
+innodb_use_atomic_writes = 1
+innodb_use_trim = 1
 
-# Install perl
-apt install perl -y
+[mysql]
+default-character-set = utf8mb4
 
-# Write MySQL pass to file and keep it safe
-echo "MySQL root password: $MYSQL_PASS" > $PW_FILE
-chmod 600 $PW_FILE
-chown root:root $PW_FILE
+[mysqld]
+innodb_large_prefix=on
+innodb_file_format=barracuda
+innodb_flush_neighbors=0
+innodb_adaptive_flushing=1
+innodb_flush_method = O_DIRECT
+innodb_doublewrite = 0
+innodb_file_per_table = 1
+innodb_flush_log_at_trx_commit=1
+init-connect='SET NAMES utf8mb4'
+collation_server=utf8mb4_unicode_ci
+character_set_server=utf8mb4
+skip-character-set-client-handshake
+LOGIN
+chmod 0600 $MYCNF
+chown root:root $MYCNF
 
-# Install MYSQL 5.7
+# Install MARIADB
 apt install software-properties-common -y
-echo "mysql-server-5.7 mysql-server/root_password password $MYSQL_PASS" | debconf-set-selections
-echo "mysql-server-5.7 mysql-server/root_password_again password $MYSQL_PASS" | debconf-set-selections
-apt install mysql-server-5.7 -y
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://ftp.ddg.lth.se/mariadb/repo/10.2/ubuntu xenial main'
+sudo debconf-set-selections <<< "mariadb-server-10.2 mysql-server/root_password password $MARIADB_PASS"
+sudo debconf-set-selections <<< "mariadb-server-10.2 mysql-server/root_password_again password $MARIADB_PASS"
+apt update -q4 & spinner_loading
+check_command apt install mariadb-server-10.2 -y
+
+# Prepare for Nextcloud installation
+# https://blog.v-gar.de/2017/02/en-solved-error-1698-28000-in-mysqlmariadb/
+mysql -u root mysql -p"$MARIADB_PASS" -e "UPDATE user SET plugin='' WHERE user='root';"
+mysql -u root mysql -p"$MARIADB_PASS" -e "UPDATE user SET password=PASSWORD('$MARIADB_PASS') WHERE user='root';"
+mysql -u root -p"$MARIADB_PASS" -e "flush privileges;"
 
 # mysql_secure_installation
 apt -y install expect
 SECURE_MYSQL=$(expect -c "
 set timeout 10
 spawn mysql_secure_installation
-expect \"Enter current password for root:\"
-send \"$MYSQL_PASS\r\"
-expect \"Would you like to setup VALIDATE PASSWORD plugin?\"
-send \"n\r\"
-expect \"Change the password for root ?\"
+expect \"Enter current password for root (enter for none):\"
+send \"$MARIADB_PASS\r\"
+expect \"Change the root password?\"
 send \"n\r\"
 expect \"Remove anonymous users?\"
 send \"y\r\"
@@ -197,6 +242,8 @@ expect eof
 echo "$SECURE_MYSQL"
 apt -y purge expect
 
+# Install VM-tools
+apt install open-vm-tools -y
 # Install Apache
 apt install apache2 -y
 a2enmod rewrite \
@@ -311,15 +358,15 @@ EOL
 
 # Change values in php.ini (increase max file size)
 # max_execution_time
-sed -i "s|max_execution_time = 30|max_execution_time = 3500|g" /etc/php/7.0/apache2/php.ini
+sed -i "s|max_execution_time =.*|max_execution_time = 3500|g" /etc/php/7.0/apache2/php.ini
 # max_input_time
-sed -i "s|max_input_time = 60|max_input_time = 3600|g" /etc/php/7.0/apache2/php.ini
+sed -i "s|max_input_time =.*|max_input_time = 3600|g" /etc/php/7.0/apache2/php.ini
 # memory_limit
-sed -i "s|memory_limit = 128M|memory_limit = 512M|g" /etc/php/7.0/apache2/php.ini
+sed -i "s|memory_limit =.*|memory_limit = 512M|g" /etc/php/7.0/apache2/php.ini
 # post_max
-sed -i "s|post_max_size = 8M|post_max_size = 1100M|g" /etc/php/7.0/apache2/php.ini
+sed -i "s|post_max_size =.*|post_max_size = 1100M|g" /etc/php/7.0/apache2/php.ini
 # upload_max
-sed -i "s|upload_max_filesize = 2M|upload_max_filesize = 1000M|g" /etc/php/7.0/apache2/php.ini
+sed -i "s|upload_max_filesize =.*|upload_max_filesize = 1000M|g" /etc/php/7.0/apache2/php.ini
 
 # Install Figlet
 apt install figlet -y
@@ -491,14 +538,36 @@ apt dist-upgrade -y
 # Remove LXD (always shows up as failed during boot)
 apt purge lxd -y
 
-#Cleanup
-CLEARBOOT=$(dpkg -l linux-* | awk '/^ii/{ print $2}' | grep -v -e `uname -r | cut -f1,2 -d"-"` | grep -e [0-9] | xargs sudo apt -y purge)
+# Cleanup
+CLEARBOOT=$(dpkg -l linux-* | awk '/^ii/{ print $2}' | grep -v -e ''"$(uname -r | cut -f1,2 -d"-")"'' | grep -e '[0-9]' | xargs sudo apt -y purge)
 echo "$CLEARBOOT"
+apt autoremove -y
+apt autoclean
+find /root "/home/$UNIXUSER" -type f \( -name '*.sh*' -o -name '*.html*' -o -name '*.tar*' -o -name '*.zip*' \) -delete
+
+# Install virtual kernels for Hyper-V, and extra for UTF8 kernel module + Collabora and OnlyOffice
+if [[ "no" == $(ask_yes_or_no "Is this installed on Hyper-V? (4.4 or 4.8 kernel)") ]]
+then
+    # Kernel 4.4
+    apt install --install-recommends -y \
+    linux-virtual-lts-xenial \
+    linux-tools-virtual-lts-xenial \
+    linux-cloud-tools-virtual-lts-xenial \
+    linux-image-virtual-lts-xenial \
+    linux-image-extra-"$(uname -r)"
+else
+    # Kernel 4.8
+    apt install --install-recommends -y \
+    linux-virtual-hwe-16.04 \
+    linux-tools-virtual-hwe-16.04 \
+    linux-cloud-tools-virtual-hwe-16.04 \
+    linux-image-virtual-hwe-16.04 \
+    linux-image-extra-"$(uname -r)"
+fi
 
 # Prefer IPv6
 sed -i "s|precedence ::ffff:0:0/96  100|#precedence ::ffff:0:0/96  100|g" /etc/gai.conf
 
 # Reboot
+echo "Installation done, system will now reboot..."
 reboot
-
-exit 0
