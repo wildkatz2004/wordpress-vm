b1cc4dca496cc2b9c29a38847db33577db0c196c
wildkatz2004 <38220800+wildkatz2004@users.noreply.github.com>
2018-04-10 16:21:41 -0500

    Update webmin.sh

--- apps/webmin.sh
+++ apps/webmin.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Tech and Me © - 2017, https://www.techandme.se/
-
 # shellcheck disable=2034,2059
 true
 # shellcheck source=lib.sh
-. <(curl -sL https://raw.githubusercontent.com/techandme/wordpress-vm/master/lib.sh)
+. <(curl -sL https://raw.githubusercontent.com/wildkatz2004/wordpress-vm/master/lib.sh)
 
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
