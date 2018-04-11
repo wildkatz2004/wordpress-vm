361e4762ea55fd07aa90d38433e54638d314806c
wildkatz2004 <38220800+wildkatz2004@users.noreply.github.com>
2018-04-11 13:55:17 -0500

    Update wordpress_install.sh

--- wordpress_install.sh
+++ wordpress_install.sh
@@ -94,8 +94,6 @@ chown root:root $MYCNF
 
 # Install MARIADB
 apt install software-properties-common -y
-sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
-sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://ftp.ddg.lth.se/mariadb/repo/10.2/ubuntu xenial main'
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $MARIADB_PASS"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MARIADB_PASS"
 apt update -q4 & spinner_loading
