6456bf5a5b5371fbb7513c484e33e78f6b8782b0
wildkatz2004 <38220800+wildkatz2004@users.noreply.github.com>
2018-04-12 10:50:08 -0500

    Update instruction.sh

--- static/instruction.sh
+++ static/instruction.sh
@@ -5,7 +5,8 @@ clear
 cat << WELCOME
 
 +-----------------------------------------------------------------------+
-|    Thank you for downloading this Wordpress VM made by Tech and Me!	|
+|    Thank you for downloading this forked Wordpress                    |
+|    VM originally made by Tech and Me!	                                |
 |									|
 |    This WordPress appliance comes with PHP 7 and Redis Cache,		|
 |    for best performance. The site(s) load very fast!			|
@@ -13,7 +14,7 @@ cat << WELCOME
 |    In order to configure everything you have to run the  setup script |
 |    To run the setup script, just type: wordpress			|
 |									|
-|    If you have any questions please email me: daniel@techandme.se	|
+|    If you have any questions please email me: wildkatz2004@outlook.com	|
 +-----------------------------------------------------------------------+
 
 WELCOME
