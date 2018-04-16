8c3e5e487a2f5bcce74191680b02b67e603e1d10
wildkatz2004 <38220800+wildkatz2004@users.noreply.github.com>
2018-04-16 14:50:21 -0500

    Update redis-server-ubuntu16.sh

--- static/redis-server-ubuntu16.sh
+++ static/redis-server-ubuntu16.sh
@@ -40,6 +40,8 @@ then
     mkdir -p $SCRIPTS
 fi
 
+# Install Redis
+
 # Get packages to be able to install Redis
 apt update -q4 & spinner_loading
 sudo apt install -q -y \
@@ -48,6 +50,60 @@ sudo apt install -q -y \
     php7.0-dev \
     php-pear
 
+
+# Redis: Download and Extract the Source Code
+cd /tmp
+curl -O http://download.redis.io/redis-stable.tar.gz
+tar xzvf redis-stable.tar.gz
+cd redis-stable
+
+# Build and Install Redis
+make
+make test
+make install
+make install PREFIX=/usr
+mkdir /etc/redis
+sudo cp /tmp/redis-stable/redis.conf /etc/redis
+cd ..
+rm -R redis*
+
+adduser --system --group --disabled-login redis --no-create-home --shell /bin/nologin --quiet
+usermod -g www-data redis
+mkdir /var/lib/redis
+chown redis:redis /var/lib/redis
+chmod 770 /var/lib/redis
+
+sed -i "s|# unixsocket /var/run/redis/redis.sock|unixsocket $REDIS_SOCK|g" $REDIS_CONF
+sed -i "s|# unixsocketperm 700|unixsocketperm 777|g" $REDIS_CONF
+sed -i "s|port 6379|port 0|g" $REDIS_CONF
+sed -i "s|# requirepass foobared|requirepass $(cat /tmp/redis_pass.txt)|g" $REDIS_CONF
+sed -i "s|# supervised no|supervised systemd|g" $REDIS_CONF
+sed -i "s|# daemonize no|daemonize yes|g" $REDIS_CONF
+sed -i "s|# maxmemory <bytes>|maxmemory 1288490188|g" $REDIS_CONF
+sed -i "s|# maxmemory-policy noeviction|maxmemory-policy allkeys-lru|g" $REDIS_CONF
+sed -i "s|# bind 127.0.0.1|bind 127.0.0.1|g" $REDIS_CONF
+sed -i" s/^dir \.\//dir \/var\/lib\/redis\//" $REDIS_CONF
+sed -i "s/^loglevel verbose$/loglevel notice/" $REDIS_CONF
+sed -i "s/^logfile stdout$/logfile \/var\/log\/redis.log/" $REDIS_CONF
+
+# Create a Redis systemd Unit File
+cat << EOF > /etc/systemd/system/redis.service
+[Unit]
+Description=Redis In-Memory Data Store
+After=network.target
+
+[Service]
+User=redis
+Group=redis
+ExecStart=/usr/local/bin/redis-server /etc/redis/redis.conf
+ExecStop=/usr/local/bin/redis-cli shutdown
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo apt-get -y install php-redis
 # Install PHPmodule
 if ! pecl install -Z redis
 then
@@ -66,33 +122,33 @@ echo 'extension=redis.so' >> /etc/php/7.0/apache2/php.ini
 service apache2 restart
 
 
-# Install Redis
-if ! apt -y install redis-server
-then
-    echo "Installation failed."
-    sleep 3
-    exit 1
-else
-    printf "${Green}\nRedis installation OK!${Color_Off}\n"
-fi
+
 
 # Redis performance tweaks
 if ! grep -Fxq "vm.overcommit_memory = 1" /etc/sysctl.conf
 then
     echo 'vm.overcommit_memory = 1' >> /etc/sysctl.conf
 fi
-sed -i "s|# unixsocket /var/run/redis/redis.sock|unixsocket $REDIS_SOCK|g" $REDIS_CONF
-sed -i "s|# unixsocketperm 700|unixsocketperm 777|g" $REDIS_CONF
-sed -i "s|port 6379|port 0|g" $REDIS_CONF
-sed -i "s|# requirepass foobared|requirepass $(cat /tmp/redis_pass.txt)|g" $REDIS_CONF
+
 redis-cli SHUTDOWN
 rm -f /tmp/redis_pass.txt
 
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
+echo 'never' > /sys/kernel/mm/transparent_hugepage/enabled
 
 # Secure Redis
 chown redis:root /etc/redis/redis.conf
 chmod 600 /etc/redis/redis.conf
+
+# Start Redis
+systemctl start redis
+sudo systemctl status redis
+sleep 5
+# Enable Redis to Start at Boot
+systemctl enable redis
+
+# Clean
+rm -rf /tmp/redis-stable
+rm /tmp/redis-stable.tar.gz
 
 # Cleanup
 apt purge -y \
