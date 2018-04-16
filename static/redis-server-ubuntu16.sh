4642cd7dbbace1b8dfdf716036de99b95f5688d5
wildkatz2004 <38220800+wildkatz2004@users.noreply.github.com>
2018-04-16 17:06:10 -0500

    Update redis-server-ubuntu16.sh

--- static/redis-server-ubuntu16.sh
+++ static/redis-server-ubuntu16.sh
@@ -4,7 +4,6 @@ true
 # shellcheck source=lib.sh
 . <(curl -sL https://raw.githubusercontent.com/wildkatz2004/wordpress-vm/master/lib.sh)
 
-# Tech and Me © - 2017, https://www.techandme.se/
 
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
@@ -40,7 +39,137 @@ then
     mkdir -p $SCRIPTS
 fi
 
-# Install Redis
+#############################################################################
+tune_memory()
+{
+	echo "Tuning the memory configuration"
+	
+	# Get the supporting utilities
+	apt-get -y install hugepages
+
+	# Resolve a "Background save may fail under low memory condition." warning
+	sysctl vm.overcommit_memory=1
+
+	# Disable the Transparent Huge Pages (THP) support in the kernel
+	sudo hugeadm --thp-never
+}
+
+#############################################################################
+
+tune_network()
+{
+	echo "Tuning the network configuration"
+	
+>/etc/sysctl.conf cat << EOF 
+	# Disable syncookies (syncookies are not RFC compliant and can use too muche resources)
+	net.ipv4.tcp_syncookies = 0
+	# Basic TCP tuning
+	net.ipv4.tcp_keepalive_time = 600
+	net.ipv4.tcp_synack_retries = 3
+	net.ipv4.tcp_syn_retries = 3
+	# RFC1337
+	net.ipv4.tcp_rfc1337 = 1
+	# Defines the local port range that is used by TCP and UDP to choose the local port
+	net.ipv4.ip_local_port_range = 1024 65535
+	# Log packets with impossible addresses to kernel log
+	net.ipv4.conf.all.log_martians = 1
+	# Disable Explicit Congestion Notification in TCP
+	net.ipv4.tcp_ecn = 0
+	# Enable window scaling as defined in RFC1323
+	net.ipv4.tcp_window_scaling = 1
+	# Enable timestamps (RFC1323)
+	net.ipv4.tcp_timestamps = 1
+	# Enable select acknowledgments
+	net.ipv4.tcp_sack = 1
+	# Enable FACK congestion avoidance and fast restransmission
+	net.ipv4.tcp_fack = 1
+	# Allows TCP to send "duplicate" SACKs
+	net.ipv4.tcp_dsack = 1
+	# Controls IP packet forwarding
+	net.ipv4.ip_forward = 0
+	# No controls source route verification (RFC1812)
+	net.ipv4.conf.default.rp_filter = 0
+	# Enable fast recycling TIME-WAIT sockets
+	net.ipv4.tcp_tw_recycle = 1
+	net.ipv4.tcp_max_syn_backlog = 20000
+	# How may times to retry before killing TCP connection, closed by our side
+	net.ipv4.tcp_orphan_retries = 1
+	# How long to keep sockets in the state FIN-WAIT-2 if we were the one closing the socket
+	net.ipv4.tcp_fin_timeout = 20
+	# Don't cache ssthresh from previous connection
+	net.ipv4.tcp_no_metrics_save = 1
+	net.ipv4.tcp_moderate_rcvbuf = 1
+	# Increase Linux autotuning TCP buffer limits
+	net.ipv4.tcp_rmem = 4096 87380 16777216
+	net.ipv4.tcp_wmem = 4096 65536 16777216
+	# increase TCP max buffer size
+	net.core.rmem_max = 16777216
+	net.core.wmem_max = 16777216
+	net.core.netdev_max_backlog = 2500
+	# Increase number of incoming connections
+	net.core.somaxconn = 65000
+EOF
+
+	# Reload the networking settings
+	/sbin/sysctl -p /etc/sysctl.conf
+}
+
+#############################################################################
+install_redis()
+{
+	log "Installing Redis"
+
+	# Installing build essentials (if missing) and other required tools
+	apt-get -y install build-essential tcl
+
+    # Redis: Download and Extract the Source Code
+    cd /tmp
+    curl -O http://download.redis.io/redis-stable.tar.gz
+    tar xzvf redis-stable.tar.gz
+    cd redis-stable
+
+    # Build and Install Redis
+    make
+    make test
+    make install
+
+    mkdir /etc/redis
+    cp /tmp/redis-stable/redis.conf /etc/redis
+
+
+	echo "Redis package was downloaded and built successfully"
+}
+
+#############################################################################
+
+configure_redis()
+{
+# Configure the general settings
+sed -i "s|# unixsocket /var/run/redis/redis.sock|unixsocket $REDIS_SOCK|g" $REDIS_CONF
+sed -i "s|# unixsocketperm 700|unixsocketperm 777|g" $REDIS_CONF
+sed -i "s|port 6379|port 0|g" $REDIS_CONF
+sed -i "s|# requirepass foobared|requirepass $(cat /tmp/redis_pass.txt)|g" $REDIS_CONF
+sed -i "s|supervised no|supervised systemd|g" $REDIS_CONF
+sed -i "s|daemonize no|daemonize yes|g" $REDIS_CONF
+sed -i "s|# maxmemory <bytes>|maxmemory 250mb|g" $REDIS_CONF
+sed -i "s|# maxmemory-policy noeviction|maxmemory-policy allkeys-lru|g" $REDIS_CONF
+sed -i "s|# bind 127.0.0.1|bind 127.0.0.1|g" $REDIS_CONF
+sed -i" s/^dir \.\//dir \/var\/lib\/redis\//" $REDIS_CONF
+sed -i "s/^loglevel verbose$/loglevel notice/" $REDIS_CONF
+sed -i "s/^logfile stdout$/logfile \/var\/log\/redis.log/" $REDIS_CONF
+}
+#############################################################################
+
+start_redis()
+{
+# Start Redis
+systemctl start redis
+sudo systemctl status redis
+sleep 5
+# Enable Redis to Start at Boot
+systemctl enable redis
+}
+#############################################################################
 
 # Get packages to be able to install Redis
 apt update -q4 & spinner_loading
@@ -49,33 +178,16 @@ sudo apt install -q -y \
     tcl8.5 \
     php7.0-dev \
     php-pear
+    
+# Step1
+tune_memory
+tune_network
 
+# Step 2
+install_redis
 
-# Redis: Download and Extract the Source Code
-cd /tmp
-curl -O http://download.redis.io/redis-stable.tar.gz
-tar xzvf redis-stable.tar.gz
-cd redis-stable
-
-# Build and Install Redis
-make
-make test
-make install
-mkdir /etc/redis
-cp /tmp/redis-stable/redis.conf /etc/redis
-
-sed -i "s|# unixsocket /var/run/redis/redis.sock|unixsocket $REDIS_SOCK|g" $REDIS_CONF
-sed -i "s|# unixsocketperm 700|unixsocketperm 777|g" $REDIS_CONF
-sed -i "s|port 6379|port 0|g" $REDIS_CONF
-sed -i "s|# requirepass foobared|requirepass $(cat /tmp/redis_pass.txt)|g" $REDIS_CONF
-sed -i "s|# supervised no|supervised systemd|g" $REDIS_CONF
-sed -i "s|# daemonize no|daemonize yes|g" $REDIS_CONF
-sed -i "s|# maxmemory <bytes>|maxmemory 250mb|g" $REDIS_CONF
-sed -i "s|# maxmemory-policy noeviction|maxmemory-policy allkeys-lru|g" $REDIS_CONF
-sed -i "s|# bind 127.0.0.1|bind 127.0.0.1|g" $REDIS_CONF
-sed -i" s/^dir \.\//dir \/var\/lib\/redis\//" $REDIS_CONF
-sed -i "s/^loglevel verbose$/loglevel notice/" $REDIS_CONF
-sed -i "s/^logfile stdout$/logfile \/var\/log\/redis.log/" $REDIS_CONF
+# Step 3
+configure_redis
 
 # Create a Redis systemd Unit File
 cat << EOF > /etc/systemd/system/redis.service
@@ -140,11 +252,7 @@ chown redis:root /etc/redis/redis.conf
 chmod 600 /etc/redis/redis.conf
 
 # Start Redis
-systemctl start redis
-sudo systemctl status redis
-sleep 5
-# Enable Redis to Start at Boot
-systemctl enable redis
+start_redis
 
 # Clean
 rm -rf /tmp/redis-stable
