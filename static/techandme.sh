d258d97607abcdf1cd3d1ca379b900de2376a174
wildkatz2004 <38220800+wildkatz2004@users.noreply.github.com>
2018-04-19 12:45:45 -0500

    Update techandme.sh

--- static/techandme.sh
+++ static/techandme.sh
@@ -3,16 +3,17 @@ WANIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 ADDRESS=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 WPADMINUSER=$(grep "WP USER:" /var/adminpass.txt)
 WPADMINPASS=$(grep "WP PASS:" /var/adminpass.txt)
+MYSQLVER=$(mysql --version|awk '{ print $5 }'|awk -F\, '{ print $1 }')
 clear
 figlet -f small D&B Consulting
 echo "           http://duanebritting.com/"
 echo
 # Check php version
- php -v
+ php -i | grep 'PHP Version'
 # Check apache version
 apachectl -v
 # Check mysql version
-mysql --version
+echo "MySQL Ver: $MYSQLVER"
 echo "|NETWORK|"
 echo "WAN IP: $WANIP"
 echo "LAN IP: $ADDRESS"
