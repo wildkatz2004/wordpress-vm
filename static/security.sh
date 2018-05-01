cdd5ac2a97a3fe3624c6b236d6e6e0828feb9b15
wildkatz2004 <38220800+wildkatz2004@users.noreply.github.com>
2018-05-01 17:03:04 -0500

    Update security.sh

--- static/security.sh
+++ static/security.sh
@@ -72,34 +72,49 @@ fi
 sed -i "s|#MS_WhiteList /etc/spamhaus.wl|MS_WhiteList $SPAMHAUS|g" /etc/apache2/mods-enabled/spamhaus.conf
 
 # Install mod_security
-apt update -q4 & spinner_loading
-apt -y install libapache2-modsecurity
-mv /etc/modsecurity/modsecurity.conf-recommended /etc/modsecurity/modsecurity.conf
+apt-get -y install libxml2 libxml2-dev libxml2-utils libaprutil1 libaprutil1-dev libapache2-modsecurity
+
 check_command systemctl restart apache2
-
-/etc/apache2/mods-enabled/security2.conf
-
 rm -rf /usr/share/modsecurity-crs
-
+apt-get install -y git
 git clone https://github.com/SpiderLabs/owasp-modsecurity-crs.git /usr/share/modsecurity-crs
 
-cd /usr/share/modsecurity-crs 
+if [ -f  /usr/share/modsecurity-crs/modsecurity_crs_10_setup.conf ]; then
+        cp /usr/share/modsecurity-crs/modsecurity_crs_10_setup.conf /usr/share/modsecurity-crs/backup/modsecurity_crs_10_setup.conf.bak
+fi
+if [ -f  /usr/share/modsecurity-crs/modsecurity_crs_10_setup.conf.example  ]; then
+        cp /usr/share/modsecurity-crs/modsecurity_crs_10_setup.conf.example /usr/share/modsecurity-crs/modsecurity_crs_10_setup.conf
+fi
+sudo 
 
-mv crs-setup.conf.example crs-setup.conf
+sudo ln -s ../modsecurity_crs_10_setup.conf activated_rules/modsecurity_crs_10_setup.conf
+for f in `ls base_rules`; do sudo ln -s ../base_rules/$f activated_rules/$f; done
+
+if [ -f /etc/modsecurity/modsecurity.conf-recommended ]; then
+       cp /etc/modsecurity/modsecurity.conf-recommended /etc/modsecurity/modsecurity.conf
+fi
+
 
 if [ -f  /etc/apache2/mods-enabled/security2.conf ]; then
         rm /etc/apache2/mods-enabled/security2.conf 
 fi
 
-cat > /etc/apache2/mods-enabled/security2.conf  << EOF 
- <IfModule security2_module> 
-     SecDataDir /var/cache/modsecurity 
-     IncludeOptional /etc/modsecurity/*.conf 
-     IncludeOptional "/usr/share/modsecurity-crs/*.conf 
-     IncludeOptional "/usr/share/modsecurity-crs/rules/*.conf 
- </IfModule>
+cat >> /etc/apache2/mods-enabled/security2.conf  << EOF 
+ <IfModule security2_module>
+	# Default Debian dir for modsecurity's persistent data
+	SecDataDir /var/cache/modsecurity
+	# Include all the *.conf files in /etc/modsecurity.
+	# Keeping your local configuration in that directory
+	# will allow for an easy upgrade of THIS file and
+	# make your life easier
+        IncludeOptional /etc/modsecurity/*.conf
+	IncludeOptional /usr/share/modsecurity-crs/rules/*.conf
+        # Include OWASP ModSecurity CRS rules if installed
+	IncludeOptional /usr/share/modsecurity-crs/owasp-crs.load
+</IfModule>
 EOF
-
+check_commanda2enmod headers
+check_commanda2enmod security2
 check_command systemctl restart apache2
 echo "Security added!"
 sleep 3
