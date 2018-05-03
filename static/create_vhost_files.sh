c86c2a46cb8e1ae54ed624ba8e8dc6ee3d5fafbc
wildkatz2004 <38220800+wildkatz2004@users.noreply.github.com>
2018-05-03 10:23:50 -0500

    Create create_vhost_files.sh

---
+++ static/create_vhost_files.sh
@@ -0,0 +1,66 @@
+# Generate $SSL_CONF
+if [ ! -f $SSL_CONF ];
+        then
+        touch $SSL_CONF
+        cat << SSL_CREATE > $SSL_CONF
+<VirtualHost *:443>
+    Header add Strict-Transport-Security: "max-age=15768000;includeSubdomains"
+    SSLEngine on
+
+### YOUR SERVER ADDRESS ###
+#    ServerAdmin admin@example.com
+#    ServerName example.com
+#    ServerAlias www.example.com
+
+### SETTINGS ###
+    DocumentRoot $WPATH
+    <Directory $WPATH>
+    Options Indexes FollowSymLinks MultiViews
+    AllowOverride All
+    Require all granted
+    </Directory>
+
+    SetEnv HOME $WPATH
+    SetEnv HTTP_HOME $WPATH
+
+### LOCATION OF CERT FILES ###
+    SSLCertificateFile /etc/ssl/certs/ssl-cert-snakeoil.pem
+    SSLCertificateKeyFile /etc/ssl/private/ssl-cert-snakeoil.key
+
+#    SSLCertificateFile /etc/ssl/example.com/certificate.crt
+#    SSLCertificateKeyFile /etc/ssl/example.com/ssl_example_com_se.key
+#    SSLCACertificateFile /etc/ssl/example.com/certificate.ca.crt
+#    SSLCertificateChainFile /etc/crt/example_com.ca-bundle
+
+</VirtualHost>
+SSL_CREATE
+echo "$SSL_CONF was successfully created"
+sleep 3
+fi
+
+# Generate $HTTP_CONF
+if [ ! -f $HTTP_CONF ];
+        then
+        touch $HTTP_CONF
+        cat << HTTP_CREATE > $HTTP_CONF
+
+<VirtualHost *:80>
+
+### YOUR SERVER ADDRESS ###
+#    ServerAdmin admin@example.com
+#    ServerName example.com
+#    ServerAlias www.example.com
+
+### SETTINGS ###
+    DocumentRoot $WPATH
+    <Directory $WPATH>
+    Options Indexes FollowSymLinks MultiViews
+    AllowOverride All
+    Require all granted
+    </Directory>
+
+</VirtualHost>
+HTTP_CREATE
+echo "$HTTP_CONF was successfully created"
+sleep 3
+fi
