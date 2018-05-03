4a6a651799ee0cb67ed32c72f45e07dba7d6c8e2
wildkatz2004 <38220800+wildkatz2004@users.noreply.github.com>
2018-05-03 18:09:09 -0500

    Update wordpress-startup-script.sh

--- wordpress-startup-script.sh
+++ wordpress-startup-script.sh
@@ -100,16 +100,16 @@ cat << EOMSTART
 |   This script will do the final setup for you                 |
 |   Scipt was created to configure Unbuntu 16.04                |   
 |                                                               |
-|   - Genereate new server SSH keys				                |
+|   - Genereate new server SSH keys                             |
 |                                                               |
 |   - Create a new WP user                                      |
 |   - Upgrade the system                                        |
 |   - Activate SSL (Let's Encrypt)                              |
-|   - Install phpMyadmin				                        |
+|   - Install phpMyadmin                                        |
 |                                                               |
 |                                                               |
 |   - Set new password to the Linux system (user: wordpress)	|
-|								                                |
+|                                                               |
 |    ################# Wordpress - 2018 #################	    |
 +---------------------------------------------------------------+
 EOMSTART
