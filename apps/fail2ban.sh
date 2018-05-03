79edcde96c75aa81eb5ca66fc46e248bb3a9dd63
wildkatz2004 <38220800+wildkatz2004@users.noreply.github.com>
2018-05-03 14:26:16 -0500

    Update fail2ban.sh

--- apps/fail2ban.sh
+++ apps/fail2ban.sh
@@ -103,7 +103,6 @@ bantime  = 172800
 maxretry = 1
 EOT
 
-sed -i "s|SecAuditLogRelevantStatus |#SecAuditLogRelevantStatus |g" /etc/modsecurity/modsecurity.conf
 # Update settings
 check_command update-rc.d fail2ban defaults
 check_command update-rc.d fail2ban enable
