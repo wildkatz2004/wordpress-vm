2993612fd4b52af554a9abbf8534575196611b6e
wildkatz2004 <38220800+wildkatz2004@users.noreply.github.com>
2018-05-03 13:11:14 -0500

    Update create_vhost_files.sh

--- static/create_vhost_files.sh
+++ static/create_vhost_files.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+# shellcheck disable=2034,2059
+true
+# shellcheck source=lib.sh
+ . <(curl -sL https://raw.githubusercontent.com/wildkatz2004/wordpress-vm/master/lib.sh)
+
+# Check for errors + debug code and abort if something isn't right
+# 1 = ON
+# 0 = OFF
+DEBUG=0
+debug_mode
+
 # Generate $SSL_CONF
 if [ ! -f $SSL_CONF ];
         then
