6a9078fa581a63be6f3a6a324dabb799f69c06d1
wildkatz2004 <38220800+wildkatz2004@users.noreply.github.com>
2018-05-04 12:44:36 -0500

    Update lamp_install.sh

--- static/lamp_install.sh
+++ static/lamp_install.sh
@@ -103,7 +103,7 @@ install_apache_depends(){
 install_apache(){
 
     #Install Apache
-    check_command apt install apache2 -y
+    check_command sudo apt install apache2 libapache2-mod-fastcgi -y
     
     #Enable Apache Modules
     a2enmod rewrite \
@@ -112,6 +112,17 @@ install_apache(){
             setenvif 
 }
 
+configure_apache(){
+
+    #Install Apache
+    check_command sudo apt install apache2 libapache2-mod-fastcgi -y
+    
+    #Enable Apache Modules
+    a2enmod rewrite \
+            headers \
+            ssl \
+            setenvif 
+}
 
 # Install PHP Dependencies Function
 install_php_depends(){
@@ -122,7 +133,7 @@ install_php_depends(){
             libldap-2.4-2 libldap2-dev libsasl2-dev libsasl2-modules-ldap libc-client2007e-dev libkrb5-dev
             autoconf2.13 pkg-config libxslt1-dev zlib1g-dev libpcre3-dev libtool unixodbc-dev libtidy-dev
             libjpeg-dev libpng-dev libfreetype6-dev libpspell-dev libmhash-dev libenchant-dev libmcrypt-dev
-            libcurl4-gnutls-dev libwebp-dev libxpm-dev libvpx-dev libreadline-dev snmp libsnmp-dev
+            libcurl4-gnutls-dev libwebp-dev libxpm-dev libvpx-dev libreadline-dev snmp libsnmp-dev python-software-properties
         )
         log "Info" "Starting to install dependencies packages for PHP..."
         for depend in ${apt_depends[@]}
@@ -151,6 +162,30 @@ install_php(){
      fi
 }
 
+# Install PHP Function
+install_php7_2(){
+
+	apt update
+	add-apt-repository ppa:ondrej/php
+	if check_sys packageManager apt; then
+		apt_php_package=(
+			php7.2 	php7.2-fpm php7.2-common php7.2-mbstring php7.2-xmlrpc php7.2-gd php7.2-xml
+			php7.2-mysql php7.2-cli php7.2-zip php7.2-curl
+			libapache2-mod-php libapache2-mod-fastcgi
+		)
+		log "Info" "Starting to install primary packages for PHP..."
+		for depend in ${apt_php_package[@]}
+		do
+		    error_detect_depends "apt-get -y install ${depend}"
+		done
+		log "Info" "Install dependencies packages for PHP completed..."
+	fi
+	
+# Configure PHP
+configure_php_7_2
+
+
+}
 #create mysql cnf
 create_php_fpm_conf(){
 
@@ -170,9 +205,27 @@ EOF
 
 }
 
+create_php_fpm_7_2_conf(){
+
+cat > /etc/apache2/conf-available/php7.2-fpm.conf << EOF 
+    <IfModule mod_fastcgi.c>
+      AddHandler php.fcgi .php
+      Action php.fcgi /php.fcgi
+      Alias /php.fcgi /usr/lib/cgi-bin/php.fcgi
+      FastCgiExternalServer /usr/lib/cgi-bin/php.fcgi -socket /run/php/php7.2-fpm.sock -pass-header Authorization -idle-timeout 3600
+      <Directory /usr/lib/cgi-bin>
+        Require all granted
+      </Directory>
+    </IfModule>
+EOF
+
+    log "Info" "create php7.0-fpm.conf file at /etc/apache2/conf-available/php7.0-fpm.conf completed."
+
+}
+
 # Configure PHP Function
 configure_php(){
-    
+    a2enmod actions fastcgi alias proxy_fcgi
     # Configure PHP
     sed -i "s|allow_url_fopen =.*|allow_url_fopen = On|g" /etc/php/7.0/fpm/php.ini
     sed -i "s|max_execution_time =.*|max_execution_time = 360|g" /etc/php/7.0/fpm/php.ini
@@ -203,6 +256,36 @@ configure_php(){
 
 }
 
+# Configure PHP Function
+configure_php_7_2(){
+	a2enmod actions fastcgi alias proxy_fcgi
+	# Configure PHP
+	sed -i "s|allow_url_fopen =.*|allow_url_fopen = On|g" /etc/php/7.2/fpm/php.ini
+	sed -i "s|max_execution_time =.*|max_execution_time = 360|g" /etc/php/7.2/fpm/php.ini
+	sed -i "s|file_uploads =.*|file_uploads = On|g" /etc/php/7.2/fpm/php.ini
+	sed -i "s|upload_max_filesize =.*|upload_max_filesize = 100M|g" /etc/php/7.2/fpm/php.ini
+	sed -i "s|memory_limit =.*|memory_limit = 256M|g" /etc/php/7.2/fpm/php.ini
+	sed -i "s|post_max_size =.*|post_max_size = 110M|g" /etc/php/7.2/fpm/php.ini
+	sed -i "s|cgi.fix_pathinfo =.*|cgi.fix_pathinfo=0|g" /etc/php/7.2/fpm/php.ini
+	sed -i "s|date.timezone =.*|date.timezone = Europe/Stockholm|g" /etc/php/7.2/fpm/php.ini	
+
+    if [ -f  /etc/apache2/conf-available/php7.2-fpm.conf ]; then
+          rm /etc/apache2/conf-available/php7.2-fpm.conf
+    fi
+
+    create_php_fpm_7_2_conf
+    
+    sudo a2dismod mpm_prefork mpm_event
+    a2enmod actions fastcgi alias proxy_fcgi mpm_worker
+    sudo a2enconf php7.2-fpm 
+
+    # And disable this module, which is mod_php:
+    sudo a2dismod php7.2
+    # Restart Apache
+    sudo service apache2 restart && sudo service php7.2-fpm restart
+
+}
+
 # Install Lamp
 lamp(){
 
@@ -210,8 +293,9 @@ lamp(){
   #install_apache_depends
   install_apache
   #install_php_depends
-  install_php
-  configure_php
+  #install_php
+  install_php7_2
+  #configure_php
 
 }
 
