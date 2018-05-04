f7d356730d52bd8157b0b9fdda715e212e8e491e
wildkatz2004 <38220800+wildkatz2004@users.noreply.github.com>
2018-05-04 16:59:22 -0500

    Update lib.sh

--- lib.sh
+++ lib.sh
@@ -142,11 +142,11 @@ PLAIN='\033[0m'
 
 log(){
     if   [ "${1}" == "Warning" ]; then
-        echo -e "[${Yellow}${1}${PLAIN}] ${2}"
+        echo -e "[${YELLOW}${1}${PLAIN}] ${2}"
     elif [ "${1}" == "Error" ]; then
-        echo -e "[${Red}${1}${PLAIN}] ${2}"
+        echo -e "[${RED}${1}${PLAIN}] ${2}"
     elif [ "${1}" == "Info" ]; then
-        echo -e "[${Green}${1}${PLAIN}] [${PLAIN}${2}${PLAIN}] "
+        echo -e "[${GREEN}${1}${PLAIN}] ${2}"
     else
         echo -e "[${1}] ${2}"
     fi
