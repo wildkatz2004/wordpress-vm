8a54bdf04e4694599a4938bafa2e33a9882adaf8
wildkatz2004 <38220800+wildkatz2004@users.noreply.github.com>
2018-05-07 10:39:32 -0500

    Update lamp_install.sh

--- static/lamp_install.sh
+++ static/lamp_install.sh
@@ -2,7 +2,9 @@
 # shellcheck disable=2034,2059
 true
 # shellcheck source=lib.sh
- . <(curl -sL https://raw.githubusercontent.com/wildkatz2004/wordpress-vm/master/lib.sh)
+WPDB=1 && MYCNFPW=1 . <(curl -sL https://raw.githubusercontent.com/wildkatz2004/wordpress-vm/master/lib.sh)
+unset MYCNFPW
+unset WPDB
 
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
@@ -18,6 +20,7 @@ then
 fi
 
 #Variable(s)
+php_ver_num=7.2
 phptoinstall=php7.2
 #Install MariaDB Function
 
@@ -45,10 +48,11 @@ chown root:root $MYCNF
 apt install software-properties-common -y
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://ftp.ddg.lth.se/mariadb/repo/10.2/ubuntu xenial main'
-sudo debconf-set-selections <<< "mariadb-server-10.2 mysql-server/root_password password $MARIADB_PASS"
-sudo debconf-set-selections <<< "mariadb-server-10.2 mysql-server/root_password_again password $MARIADB_PASS"
+sudo debconf-set-selections <<< "mariadb-server-10.2 mysql-server/root_password password $MARIADBMYCNFPASS"
+sudo debconf-set-selections <<< "mariadb-server-10.2 mysql-server/root_password_again password $MARIADBMYCNFPASS"
 apt update -q4 & spinner_loading
 check_command apt install mariadb-server-10.2 -y
+log "Info" "Install of MariaDB completed..."
 
 # Prepare for Wordpress installation
 # https://blog.v-gar.de/2017/02/en-solved-error-1698-28000-in-mysqlmariadb/
@@ -58,11 +62,12 @@ mysql -u root -p"$MARIADB_PASS" -e "flush privileges;"
 
 # mysql_secure_installation
 apt -y install expect
+log "Info" "Spawning mysql_secure_installation..."
 SECURE_MYSQL=$(expect -c "
 set timeout 10
 spawn mysql_secure_installation
 expect \"Enter current password for root (enter for none):\"
-send \"$MARIADB_PASS\r\"
+send \"$MARIADBMYCNFPASS\r\"
 expect \"Change the root password?\"
 send \"n\r\"
 expect \"Remove anonymous users?\"
@@ -77,7 +82,7 @@ expect eof
 ")
 echo "$SECURE_MYSQL"
 apt -y purge expect
-
+log "Info" "mysql_secure_installation config. completed..."
 # Write a new MariaDB config
 run_static_script new_etc_mycnf
 }
@@ -220,37 +225,107 @@ install_php_depends(){
 }
 
 # Install PHP Function
-install_php(){
-local phpversion=php7.0
-    if check_sys packageManager apt; then
-        apt_php_package=(
-			$phpversion $phpversion-fpm $phpversion-common $phpversion-mbstring $phpversion-xmlrpc $phpversion-gd $phpversion-xml
-			$phpversion-mysql $phpversioncli $phpversion-zip $phpversion-curl zip unzip
-			libapache2-mod-php libapache2-mod-fastcgi
-        )
-        log "Info" "Starting to install primary packages for PHP..."
-        for depend in ${apt_php_package[@]}
-        do
-            error_detect_depends "apt-get -y install ${depend}"
-        done
-        log "Info" "Install dependencies packages for PHP completed..."
-     fi
+#install_php(){
+#local phpversion=php7.0
+#    if check_sys packageManager apt; then
+#        apt_php_package=(
+#			$phpversion $phpversion-fpm $phpversion-common $phpversion-mbstring $phpversion-xmlrpc $phpversion-gd $phpversion-xml
+#			$phpversion-mysql $phpversioncli $phpversion-zip $phpversion-curl zip unzip
+#			libapache2-mod-php libapache2-mod-fastcgi
+#       )
+#        log "Info" "Starting to install primary packages for PHP..."
+#        for depend in ${apt_php_package[@]}
+#        do
+#            error_detect_depends "apt-get -y install ${depend}"
+#        done
+#        log "Info" "Install dependencies packages for PHP completed..."
+#     fi
      
-php -v
+#php -v
 
 # Lets also check if the PHP7.2-FPM is running, if not start it
-configure_php
-service $phpversion-fpm status
-if (( $(ps -ef | grep -v grep | grep $phpversion-fpm | wc -l) > 0 ))
-then
-echo "$service is running"
-else
-service $phpversion-fpm start  # (if the service isn't running already)
-fi     
+#configure_php
+#service $phpversion-fpm status
+#if (( $(ps -ef | grep -v grep | grep $phpversion-fpm | wc -l) > 0 ))
+#then
+#echo "$service is running"
+#else
+#service $phpversion-fpm start  # (if the service isn't running already)
+#fi     
+#}
+
+#create mysql cnf
+create_php_fpm_conf(){
+log "Info" "Beginning creation of $phptoinstall-fpm.conf"
+cat > /etc/apache2/conf-available/$phptoinstall-fpm.conf << EOF 
+    <IfModule mod_fastcgi.c>
+      AddHandler php.fcgi .php
+      Action php.fcgi /php.fcgi
+      Alias /php.fcgi /usr/lib/cgi-bin/php.fcgi
+      FastCgiExternalServer /usr/lib/cgi-bin/php.fcgi -socket /run/php/$phptoinstall-fpm.sock -pass-header Authorization -idle-timeout 3600
+      <Directory /usr/lib/cgi-bin>
+        Require all granted
+      </Directory>
+    </IfModule>
+EOF
+log "Info" "Completed creation of $phptoinstall-fpm.conf"
+log "Info" "create $phptoinstall-fpm.conf file at /etc/apache2/conf-available/$phptoinstall-fpm.conf completed."
+
 }
 
+# Configure PHP Function
+configure_php(){
+log "Info" "Beginning php.ini edits."
+	# Configure PHP
+	sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/$php_ver_num/cli/php.ini
+	sed -i "s/display_errors = .*/display_errors = On/" /etc/php/$php_ver_num/cli/php.ini
+	sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/$php_ver_num/cli/php.ini
+	sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/$php_ver_num/cli/php.ini
+	# Configure PHP-FPM
+	sed -i "s|allow_url_fopen =.*|allow_url_fopen = On|g" /etc/php/$php_ver_num/fpm/php.ini
+	sed -i "s|max_execution_time =.*|max_execution_time = 360|g" /etc/php/$php_ver_num/fpm/php.ini
+	sed -i "s|file_uploads =.*|file_uploads = On|g" /etc/php/$php_ver_num/fpm/php.ini
+	sed -i "s|cgi.fix_pathinfo =.*|cgi.fix_pathinfo=0|g" /etc/php/$php_ver_num/fpm/php.ini
+	sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/$php_ver_num/cli/php.ini
+	sed -i "s/display_errors = .*/display_errors = On/" /etc/php/$php_ver_num/cli/php.ini
+	sed -i "s/error_reporting = .*/error_reporting = E_ALL \& ~E_NOTICE \& ~E_STRICT \& ~E_DEPRECATED/" /etc/php/$php_ver_num/fpm/php.ini
+	sed -i "s/display_errors = .*/display_errors = Off/" /etc/php/$php_ver_num/fpm/php.ini
+	sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/$php_ver_num/fpm/php.ini
+	sed -i "s/upload_max_filesize = .*/upload_max_filesize = 256M/" /etc/php/$php_ver_num/fpm/php.ini
+	sed -i "s/post_max_size = .*/post_max_size = 256M/" /etc/php/$php_ver_num/fpm/php.ini
+	sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/$php_ver_num/fpm/php.ini
+	#Tune PHP-FPM pool settings
+	#sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.2/fpm/pool.d/www.conf
+	#sed -i "s/;request_terminate_timeout.*/request_terminate_timeout = 60/" /etc/php/7.2/fpm/pool.d/www.conf
+	#sed -i "s/pm\.max_children.*/pm.max_children = 70/" /etc/php/7.2/fpm/pool.d/www.conf
+	#sed -i "s/pm\.start_servers.*/pm.start_servers = 20/" /etc/php/7.2/fpm/pool.d/www.conf
+	#sed -i "s/pm\.min_spare_servers.*/pm.min_spare_servers = 20/" /etc/php/7.2/fpm/pool.d/www.conf
+	#sed -i "s/pm\.max_spare_servers.*/pm.max_spare_servers = 35/" /etc/php/7.2/fpm/pool.d/www.conf
+	#sed -i "s/;pm\.max_requests.*/pm.max_requests = 500/" /etc/php/7.2/fpm/pool.d/www.conf	
+	
+	#Configure sessions directory permissions
+	chmod 733 /var/lib/php/sessions
+	chmod +t /var/lib/php/sessions
+
+    if [ -f  /etc/apache2/conf-available/$phptoinstall-fpm.conf ]; then
+          rm /etc/apache2/conf-available/$phptoinstall-fpm.conf
+    fi
+
+    #Not sure about this... 
+    create_php_fpm_conf
+    #Might need to disable? 
+    sudo a2dismod $phptoinstall
+    sudo a2dismod mpm_prefork mpm_worker 
+    a2enmod mpm_event headers
+    sudo a2enconf $phptoinstall-fpm 
+    # Restart Apache
+    sudo service apache2 restart && sudo service $phptoinstall-fpm restart
+log "Info" "Php.ini edits completed."
+}
+
+
 # Install PHP Function
-install_php7_2(){
+install_php(){
 local phpversion=php7.2
 # Add Repository which gives us the latest php version 7.2
 add-apt-repository ppa:ondrej/php
@@ -274,7 +349,7 @@ apt-cache show php
 	fi
 	
 # Configure PHP
-configure_php_7_2
+configure_php
 
 php -v
 
@@ -288,122 +363,40 @@ else
 service $phpversion-fpm start  # (if the service isn't running already)
 fi
 }
-#create mysql cnf
-create_php_fpm_conf(){
-log "Info" "Beginning creation of php7.0-fpm.conf"
-cat > /etc/apache2/conf-available/php7.0-fpm.conf << EOF 
-    <IfModule mod_fastcgi.c>
-      AddHandler php.fcgi .php
-      Action php.fcgi /php.fcgi
-      Alias /php.fcgi /usr/lib/cgi-bin/php.fcgi
-      FastCgiExternalServer /usr/lib/cgi-bin/php.fcgi -socket /run/php/php7.0-fpm.sock -pass-header Authorization -idle-timeout 3600
-      <Directory /usr/lib/cgi-bin>
-        Require all granted
-      </Directory>
-    </IfModule>
-EOF
-log "Info" "Completed creation of php7.0-fpm.conf"
-log "Info" "create php7.0-fpm.conf file at /etc/apache2/conf-available/php7.0-fpm.conf completed."
 
-}
-
-create_php_fpm_7_2_conf(){
-log "Info" "Beginning creation of php7.2-fpm.conf"
-cat > /etc/apache2/conf-available/$phptoinstall-fpm.conf << EOF 
-    <IfModule mod_fastcgi.c>
-      AddHandler php.fcgi .php
-      Action php.fcgi /php.fcgi
-      Alias /php.fcgi /usr/lib/cgi-bin/php.fcgi
-      FastCgiExternalServer /usr/lib/cgi-bin/php.fcgi -socket /run/php/php7.2-fpm.sock -pass-header Authorization -idle-timeout 3600
-      <Directory /usr/lib/cgi-bin>
-        Require all granted
-      </Directory>
-    </IfModule>
-EOF
-
-log "Info" "Completed creation of php7.2-fpm.conf"
-log "Info" "create php7.2-fpm.conf file at /etc/apache2/conf-available/php7.2-fpm.conf completed."
-
-}
 
 # Configure PHP Function
-configure_php(){
-log "Info" "Beginning php.ini edits."
+#configure_php(){
+#log "Info" "Beginning php.ini edits."
     # Configure PHP
-    sed -i "s|allow_url_fopen =.*|allow_url_fopen = On|g" /etc/php/7.0/fpm/php.ini
-    sed -i "s|max_execution_time =.*|max_execution_time = 360|g" /etc/php/7.0/fpm/php.ini
-    sed -i "s|file_uploads =.*|file_uploads = On|g" /etc/php/7.0/fpm/php.ini
-    sed -i "s|upload_max_filesize =.*|upload_max_filesize = 100M|g" /etc/php/7.0/fpm/php.ini
-    sed -i "s|memory_limit =.*|memory_limit = 256M|g" /etc/php/7.0/fpm/php.ini
-    sed -i "s|post_max_size =.*|post_max_size = 110M|g" /etc/php/7.0/fpm/php.ini
-    sed -i "s|cgi.fix_pathinfo =.*|cgi.fix_pathinfo=0|g" /etc/php/7.0/fpm/php.ini
-    sed -i "s|date.timezone =.*|date.timezone = US/Central|g" /etc/php/7.0/fpm/php.ini
+    #sed -i "s|allow_url_fopen =.*|allow_url_fopen = On|g" /etc/php/7.0/fpm/php.ini
+    #sed -i "s|max_execution_time =.*|max_execution_time = 360|g" /etc/php/7.0/fpm/php.ini
+    #sed -i "s|file_uploads =.*|file_uploads = On|g" /etc/php/7.0/fpm/php.ini
+    #sed -i "s|upload_max_filesize =.*|upload_max_filesize = 100M|g" /etc/php/7.0/fpm/php.ini
+    #sed -i "s|memory_limit =.*|memory_limit = 256M|g" /etc/php/7.0/fpm/php.ini
+    #sed -i "s|post_max_size =.*|post_max_size = 110M|g" /etc/php/7.0/fpm/php.ini
+    #sed -i "s|cgi.fix_pathinfo =.*|cgi.fix_pathinfo=0|g" /etc/php/7.0/fpm/php.ini
+    #sed -i "s|date.timezone =.*|date.timezone = US/Central|g" /etc/php/7.0/fpm/php.ini
 
-    if [ -f  /etc/apache2/conf-available/php7.0-fpm.conf ]; then
-          rm /etc/apache2/conf-available/php7.0-fpm.conf
-    fi
+    #if [ -f  /etc/apache2/conf-available/php7.0-fpm.conf ]; then
+  #        rm /etc/apache2/conf-available/php7.0-fpm.conf
+  #  fi
 
-    create_php_fpm_conf
+    #create_php_fpm_conf
 
     #enable /etc/apache2/conf-available/php-fpm.confcat
-    sudo a2enconf php7.0-fpm
+    #sudo a2enconf php7.0-fpm
     
-    sudo a2dismod mpm_prefork mpm_worker 
-    a2enmod actions fastcgi alias proxy_fcgi mpm_event
+    #sudo a2dismod mpm_prefork mpm_worker 
+   # a2enmod actions fastcgi alias proxy_fcgi mpm_event
     # And disable this module, which is mod_php:
-    sudo a2dismod php7.0
+    #sudo a2dismod php7.0
     # Restart Apache
-    sudo service apache2 restart && sudo service php7.0-fpm restart
-log "Info" "Php.ini edits completed."
-}
+   #sudo service apache2 restart && sudo service php7.0-fpm restart
+#log "Info" "Php.ini edits completed."
+#}
 
-# Configure PHP Function
-configure_php_7_2(){
-log "Info" "Beginning php.ini edits."
-	# Configure PHP
-	sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.2/cli/php.ini
-	sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.2/cli/php.ini
-	sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.2/cli/php.ini
-	sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.2/cli/php.ini
-	# Configure PHP-FPM
-	sed -i "s|allow_url_fopen =.*|allow_url_fopen = On|g" /etc/php/7.2/fpm/php.ini
-	sed -i "s|max_execution_time =.*|max_execution_time = 360|g" /etc/php/7.2/fpm/php.ini
-	sed -i "s|file_uploads =.*|file_uploads = On|g" /etc/php/7.2/fpm/php.ini
-	sed -i "s|cgi.fix_pathinfo =.*|cgi.fix_pathinfo=0|g" /etc/php/7.2/fpm/php.ini
-	sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.2/cli/php.ini
-	sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.2/cli/php.ini
-	sed -i "s/error_reporting = .*/error_reporting = E_ALL \& ~E_NOTICE \& ~E_STRICT \& ~E_DEPRECATED/" /etc/php/7.2/fpm/php.ini
-	sed -i "s/display_errors = .*/display_errors = Off/" /etc/php/7.2/fpm/php.ini
-	sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.2/fpm/php.ini
-	sed -i "s/upload_max_filesize = .*/upload_max_filesize = 256M/" /etc/php/7.2/fpm/php.ini
-	sed -i "s/post_max_size = .*/post_max_size = 256M/" /etc/php/7.2/fpm/php.ini
-	sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.2/fpm/php.ini
-	#Tune PHP-FPM pool settings
-	sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.2/fpm/pool.d/www.conf
-	sed -i "s/;request_terminate_timeout.*/request_terminate_timeout = 60/" /etc/php/7.2/fpm/pool.d/www.conf
-	sed -i "s/pm\.max_children.*/pm.max_children = 70/" /etc/php/7.2/fpm/pool.d/www.conf
-	sed -i "s/pm\.start_servers.*/pm.start_servers = 20/" /etc/php/7.2/fpm/pool.d/www.conf
-	sed -i "s/pm\.min_spare_servers.*/pm.min_spare_servers = 20/" /etc/php/7.2/fpm/pool.d/www.conf
-	sed -i "s/pm\.max_spare_servers.*/pm.max_spare_servers = 35/" /etc/php/7.2/fpm/pool.d/www.conf
-	sed -i "s/;pm\.max_requests.*/pm.max_requests = 500/" /etc/php/7.2/fpm/pool.d/www.conf	
-	
-	#Configure sessions directory permissions
-	chmod 733 /var/lib/php/sessions
-	chmod +t /var/lib/php/sessions
 
-    if [ -f  /etc/apache2/conf-available/php7.2-fpm.conf ]; then
-          rm /etc/apache2/conf-available/php7.2-fpm.conf
-    fi
-
-   # Not sure about this---- create_php_fpm_7_2_conf
-    sudo a2dismod php7.2
-    sudo a2dismod mpm_prefork mpm_worker 
-    a2enmod mpm_event headers
-    sudo a2enconf php7.2-fpm 
-    # Restart Apache
-    sudo service apache2 restart && sudo service php7.2-fpm restart
-log "Info" "Php.ini edits completed."
-}
 
 # Install Lamp
 lamp(){
